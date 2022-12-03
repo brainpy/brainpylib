68a492349f394a0f651871a648335044eb31b190
chaoming <adaduo@outlook.com>
2022-12-03 11:05:11 +0800

    ready for merge

--- brainpylib/jitconn_ops/__init__.py
+++ brainpylib/jitconn_ops/__init__.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from . import (matvec,
-               event_matvec,
-               low_rank,
-               event_masked_low_rank, )
+               event_matvec,)
 
 __all__ = (matvec.__all__ +
-           event_matvec.__all__ +
-           event_masked_low_rank.__all__ +
-           low_rank.__all__)
+           event_matvec.__all__ )
 
 from .matvec import *
 from .event_matvec import *
-from .event_masked_low_rank import *
-from .low_rank import *
