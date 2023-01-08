560da984d07144ad55651fbdb438721c3b62e7f7
chaoming <adaduo@outlook.com>
2023-01-08 19:32:00 +0800

    fix bug

--- brainpylib/compat.py
+++ brainpylib/compat.py
@@ -2,6 +2,6 @@
 
 from ._src.compat.atomic_prod import (coo_atomic_prod as coo_atomic_prod)
 from ._src.compat.atomic_sum import (coo_atomic_sum as coo_atomic_sum)
-from ._src.compat.event_prod import (csr_event_prod as coo_atomic_sum)
+from ._src.compat.event_prod import (csr_event_prod as csr_event_prod)
 from ._src.compat.event_sum import (csr_event_sum as csr_event_sum,
                                     coo_event_sum as coo_event_sum)
